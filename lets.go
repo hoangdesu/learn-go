656cf212776b91fb848550fe1287535717052aa2
Brian <hoangdesu@gmail.com>
2024-02-18 04:05:39 +0700

    pointers, address, types

--- lets.go
+++ lets.go
@@ -5,8 +5,8 @@ import "fmt" // "Format package"
 func main() {
 	fmt.Print("Let's Go!")
 
-	var name = "Brian"
-	var username string = "hoangdesu"
+	var name = "Brian" // type string is auto inferred
+	var username string = "hoangdesu" // can specify var type
 
 	fmt.Println("username", username)
 
@@ -33,22 +33,43 @@ func main() {
 	password = "secret"
 	fmt.Println("it's a", password)
 
-	hobby := "coding"
+	hobby := "coding" // declare + assign
 	fmt.Println(hobby)
 
 
 	// Formatted print
 	fmt.Printf("The value of variable \"name\" is \"%v\", data type is %T\n", name, name)
-	// - can use %v for value, %T for data type
-	// - or can use C-style formatted for correct data type e.g. %s for string..., %d for int 
-	fmt.Printf("Age: %d", age)
+	// - placeholders: can use %v for value in default format, %T for data type
+	// - or can use C-style formatted for correct data type e.g. %s for string..., %d for int
+	fmt.Printf("Age: %d, data type: %T\n", age, age)
+
+	// int types: int 8 -> 64
+	// same for uint
+
+	// Pointers
+	fmt.Printf("address of password %v: %p\n", password, &password)
+	// - using %v or %p for pointer is fine, as long as we get the address of the variable with & e.g. &name
 
 
 	// User input
-	// fmt.Print("Enter new username: ")
-	// fmt.Scan(&username)
-	// fmt.Println("New username:", username)
+	fmt.Printf("Current username: %v. Enter new username: ", username)
+	fmt.Scan(&username) // - using the pointer to username's address to "pass by reference"
+	fmt.Println("New username:", username)
+
+
+	// Ex: Book tickets
+	totalTickets := 10
+
+	var tickets uint8
+
+	fmt.Print("How many tickets: ")
+	fmt.Scan(&tickets)
+
+	if int(tickets) > totalTickets {
+		fmt.Println("Error: Not enough tickets")
+	} else {
+		totalTickets -= int(tickets)
+		fmt.Printf("User %v booked %v tickets. %v tickets remaining\n", username, tickets, totalTickets)
+	}
 
 }
-
-
